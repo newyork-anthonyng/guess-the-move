347725ab3b172d5febbdcd8cba04af0c29767313
Anthony Ng <newyork.anthonyng@gmail.com>
2023-07-01 12:54:46 -0400

    feat: handle end of game

--- __tests__/guess.test.tsx
+++ __tests__/guess.test.tsx
@@ -2,6 +2,10 @@ import { render } from '@testing-library/react';
 import Page from '../src/app/guess/[gameId]/page';
 import '@testing-library/jest-dom'
 
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(),
+}))
+
 let config: any;
 let mockGetFen = jest.fn();
 let mockSet = jest.fn(a => config = { ...config, ...a });
