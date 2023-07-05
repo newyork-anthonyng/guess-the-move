0be10b784874bc5fa04ed8d89cfa88f4e53895e5
Anthony Ng <newyork.anthonyng@gmail.com>
2023-07-05 18:23:09 -0400

    feat: use real endpoints

---
+++ src/app/api/game/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+
+const API_URL = `${process.env.CHESS_EVAL_BACKEND_BASE_URL}/validate_pgn`
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { pgn } = body;
+
+  const res = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      pgn,
+      color: 'white' // for now, user can only play as white
+    })
+  });
+  const data = await res.json();
+
+  if (data.id) {
+    return NextResponse.json({ ok: true, gameId: data.id })
+  }
+
+  return NextResponse.json({ ok: false })
+}
